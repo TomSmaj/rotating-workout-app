7698f9edb943e0a98ec3b1456e367301b73ab262
Thomas Smajstrla <thomassmajstrla@Thomass-MacBook-Pro.local>
2022-10-18 14:17:37 -0500

    created board and exercise components, created data folder with exercise info

---
+++ client/src/components/Board.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        
+        }
+        
+    }
+
+    componentDidMount(){
+     
+    }
+
+    render() {
+        return (
+            <div className="exerciseBoard">
+                Exercise Board
+            </div>
+        )
+    }
+}
+
+export default Board;
