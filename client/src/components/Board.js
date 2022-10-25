dacae81c8a4a13ea54ffa784c24a94c8ff7fde10
Thomas Smajstrla <thomassmajstrla@Thomass-MacBook-Pro.local>
2022-10-25 14:06:53 -0500

    request exerciseData and exerciseOrder from server

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
-//import exercises from '../data/exercises';
-//import exerciseOrder from '../data/exercise-order';
 import $ from 'jquery';
 
 class Board extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            //exercises: exercises,
-            //exerciseOrder: exerciseOrder
-            exerciseData: {},            
+            exerciseData: {},
+            exerciseOrder: []
         }
-        
+
     }
 
-    componentDidMount(){
-        console.log("making exercise data request");
-        $.get("/get-exercise-data").then(res => {            
-            this.setState({exerciseData: res});
-        })
+    componentDidMount() {
+
+        $.get("/get-exercise-data").then(res => {
+            this.setState({ exerciseData: res });
+        }).then($.get("/get-exercise-order").then(res => {
+            this.setState({ exerciseOrder: res.order });
+        }))
     }
 
     render() {
--- routes/routes.js
+++ routes/routes.js
@@ -1,19 +1,27 @@
 const fs = require('fs')
 
 module.exports = function (app) {
-    app.get("/get-exercise-data", (req, res) => {        
+    app.get("/get-exercise-data", (req, res) => {
         fs.readFile('./data/exerciseData.json', 'utf8', (err, jsonString) => {
             if (err) {
                 return;
             }
+            const exercises = JSON.parse(jsonString);
+            res.status(200).send(exercises);
+        })
+    });
+
+    app.get("/get-exercise-order", (req, res) => {        
+        fs.readFile('./data/exerciseOrder.json', 'utf8', (err, jsonString) => {
+            if (err) {
+                return;
+            }
             try {
-                const exercises = JSON.parse(jsonString);
-                res.status(200).send(exercises);
+                const order = JSON.parse(jsonString);
+                res.status(200).send(order);
             } catch (err) {
                 console.log('Error parsing JSON string:', err);
             }
-        })
-
-        
+        })        
     })
 }
