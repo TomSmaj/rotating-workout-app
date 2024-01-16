8306bcf1b5cb2c7d33aeb1ad5247bec953392ba4
TomSmaj <thomas.smajstrla@gmail.com>
2024-01-16 12:50:42 -0600

    trying to get app to use static elements hosted in s3 through cloudfront distribution

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ dotenv.config({
 
 module.exports = {
     NODE_ENV : process.env.NODE_ENV,
+    CLOUDFRONT_DIST_DOMAIN : process.env.CLOUDFRONT_DIST_DOMAIN,
     DB_HOST : process.env.DB_HOST,
     DB_USER: process.env.DB_USER,
     DB_PASSWORD : process.env.DB_PASSWORD,
--- server.js
+++ server.js
@@ -14,7 +14,11 @@ require("./routes/routes.js")(app);
 
 console.log('are we in production?', config.NODE_ENV)
 
+app.get('*', (req, res) => {
+    res.redirect(config.CLOUDFRONT_DIST_DOMAIN);
+    // or serve an HTML that points to your CloudFront distribution
+    // res.sendFile(path.join(__dirname, 'path_to_html_pointing_to_cloudfront.html'));
+  });
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-app.use(express.static("./client/build"));
