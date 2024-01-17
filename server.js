ba51b6b65de6f6e5e4ee1a290452acd53786bfab
TomSmaj <thomas.smajstrla@gmail.com>
2024-01-16 19:38:19 -0600

    adding cors to server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const port = process.env.PORT || 3001;
 const app = express();
+const cors = require('cors');
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -16,9 +17,9 @@ console.log('are we in production?', config.NODE_ENV)
 
 app.get('*', (req, res) => {
     res.redirect(config.CLOUDFRONT_DIST_DOMAIN);
-    // or serve an HTML that points to your CloudFront distribution
-    // res.sendFile(path.join(__dirname, 'path_to_html_pointing_to_cloudfront.html'));
   });
+
+app.use(cors());
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
